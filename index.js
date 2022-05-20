bafe29edbfb718294b0af3443e64bb8cc77e2d34
Fig369 <figueroa.jorge20@gmail.com>
2022-05-19 23:45:57 -0400

    updates

--- index.js
+++ index.js
@@ -177,7 +177,8 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
-  });  
+  });
+
 /* const express = require('express');
 const app = express();
 
